b99ce0dcf9abb7b2203ff63a31c69b49c1e3f73a
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2023-11-21 00:42:25 -0500

    feat: Detect eval() calls

--- src/JavascriptVulnDetector.java
+++ src/JavascriptVulnDetector.java
@@ -1,9 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JavascriptVulnDetector extends JavaScriptParserBaseListener {
+    String evalRegex = "\\beval\\([^)]*\\)";
 
     @Override
-    public void enterExpressionSequence(JavaScriptParser.ExpressionSequenceContext ctx) {
-        if (ctx.getText().equals("eval")) {
-            System.out.println("omg");
+    public void enterExpressionStatement(JavaScriptParser.ExpressionStatementContext ctx) {
+        String input = ctx.getText();
+
+        Pattern pattern = Pattern.compile(evalRegex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            System.out.println(input);
+            System.out.println("^^^^");
+            System.out.println("The input contains an eval() call. These calls should be avoided as they allow for malicious code injection inside its parameters.");
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("JS vuln analyzer");
+    public static void main(String[] args) throws IOException {
+        System.out.println("JS vuln analyzer");
+        System.out.println("----------------");
+
+        // crear un analizador léxico
+        JavaScriptLexer lexer;
+        if (args.length==0)
+            lexer = new JavaScriptLexer(CharStreams.fromFileName("input/test_case.txt"));
+        else
+            lexer = new JavaScriptLexer(CharStreams.fromFileName(args[0]));
+
+        // Identificar al analizador léxico como fuente de tokens para el sintactico
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Crear el analizador sintáctico que se alimenta a partir del buffer de tokens
+        JavaScriptParser parser = new JavaScriptParser(tokens);
+
+        ParseTree tree = parser.program(); // comienza el análisis en la regla inicial
+        //System.out.println(tree.toStringTree(parser)); // imprime el árbol en forma textual
+
+        JavascriptVulnDetector listener = new JavascriptVulnDetector();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
+        System.out.println();
     }
 }
